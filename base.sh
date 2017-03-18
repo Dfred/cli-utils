9236c0ea421df91abf9b0d92f972d0ef8cb1ab3f
Frederic Delaunay <frederic.delaunay@manymakers.net>
2017-03-18 03:28:13 +0100

    bot_choice: fixed max_i calculation; added EXIT_NOT_IMPLEMENTED and EXIT_FATAL_ERROR; cosmetics

--- base.sh
+++ base.sh
@@ -11,7 +11,9 @@ readonly EXIT_OK=0
 ## MAYBE LEAVE ROOM FOR LSB init SCRIPTS EXIT CODES
 readonly EXIT_UNKNOWN=10
 readonly EXIT_USER_ABORT=11
-readonly EXIT_INVALID_ARGUMENT=12
+readonly EXIT_FATAL_ERROR=12
+readonly EXIT_NOT_IMPLEMENTED=13
+readonly EXIT_INVALID_ARGUMENT=14
 ## LEAVE [ 64 - 78 ] (SEE /usr/include/sysexits.h)
 #readonly EXIT_
 
@@ -53,12 +55,13 @@ readonly P_CSMC="${CBE}"                  ## LEVEL: COSMETIC (OPTIONAL)
 function fatal_prog_error () {
   echo -e "${CRD}${LARG}SCRIPT ERROR${NORM}${CRD} [$0:${BASH_LINENO[1]}] CALL"\
     TO ${FUNCNAME[1]}:${NORM} $1
-  exit 127
+  exit $EXIT_FATAL_ERROR
 }
 
 ## INVOKE SUDO/SU IF NEEDED
-function run_as () {
+function run_as ()
   ## $1: USER NAME
+{
   if ! getent passwd $1 >/dev/null 2>&1; then
     fatal_prog_error "USER '$1' DOES NOT EXIST"
   fi
@@ -70,28 +73,38 @@ function run_as () {
 }
 
 ## PROCESS_NAME TO USER_NAME PID PROCESS_NAME.
-function procname2Upca () {
+function procname2Upca ()
   ## $1: PROCESS NAME - REGEXP ALLOWED
+{
   ps ax -o '%U %p %a' | grep -E "$1"                  | grep -v grep |sort
 }
 
 ## PROCESS_NAME TO USER_NAME.
-function procname2U () {
+function procname2U ()
   ## $1: PROCESS NAME - REGEXP ALLOWED
+{
   ps ax -o '%U %c'    | grep -E "$1" | cut -f1 -d' '  | grep -v grep |sort
 }
 
 ## PROCESS_NAME TO PID.
-function procname2p () {
+function procname2p ()
   ## $1: PROCESS NAME - REGEXP ALLOWED
+{
   ps ax -o '%p %U'    | grep -E "$1" | cut -f1 -d' '  | grep -v grep |sort
 }
 
 ## GENERIC COLORISED PRINT WITH SUPPORT FOR FORMATTING.
-function p_color () {
+## EXAMPLES YOU SHOULD TRY:
+# p_color ${CMA} "This sentence is in magenta"
+# p_color ${NORM} "default," "${CCN}cyan,"  "default," "${CRD}red."
+# p_color ${NORM} "default," "${CCN}cyan,not default,   ${CRD}red."
+# p_color ${CBE} -n "Hi " "${CMPV}$(whoami)"; p_color ${CGN} " GO?"
+# p_color ${CWE} -n "this 2-call version"; p_color ${CYW} " also works!"
+function p_color ()
   ## $1   : ANY OF P_* COLORS               #XXX: E.G: "-N ${P_CRIT}"
   ## $2   : OPTIONS TO echo (OPTIONAL)
   ## $3-n : MESSAGE
+{
   local color="$1"; shift
   local opts=""
   if [[ ${1:0:1} == "-" ]]; then
@@ -102,40 +115,38 @@ function p_color () {
   done
   echo -e $opts ${NORM}
 }
-## EXAMPLES YOU SHOULD TRY:
-# p_color ${CMA} "This sentence is in magenta"
-# p_color ${NORM} "default," "${CCN}cyan,"  "default," "${CRD}red."
-# p_color ${NORM} "default," "${CCN}cyan,not default,   ${CRD}red."
-# p_color ${CBE} -n "Hi " "${CMPV}$(whoami)"; p_color ${CGN} " GO?"
-# p_color ${CWE} -n "this 2-call version"; p_color ${CYW} " also works!"
 
 ## SCRIPT HAS SOMETHING INFORMATIVE TO SAY.
-function bot_info () {
+function bot_info ()
   ## $@ : same as echo
+{
   echo -ne "$BP$B_AWAKE "
   p_color ${P_CSMC} "$@"
 }
 
 ## SCRIPT HAS SOMETHING HE'S HAPPY ABOUT.
-function bot_success () {
+function bot_success ()
   ## $@ : SAME AS echo (MESSAGE WILL BE PREFIXED)
+{
   echo -ne "$BP$B_HAPPY "
   [[ ${1:0:1} == '-' ]] && local opts="$1" && shift
   p_color ${P_SUCC} "$opts" "Good: $@"
 }
 
 ## SCRIPT HAS SOMETHING YOU SHOULD PAY ATTENTION TO.
-function bot_warn () {
+function bot_warn ()
   ## $@ : SAME AS echo (MESSAGE WILL BE PREFIXED)
+{
   echo -ne "$BP$B_UPSET "
   [[ ${1:0:1} == '-' ]] && local opts="$1" && shift
   p_color ${P_WARN} "$opts" "Hey! $@"
 }
 
 ## SCRIPT HAS SOMETHING HE'S CHOCKING ABOUT AND WILL DIE.
-function bot_fatal () {
+function bot_fatal ()
   ## $1   : exit CODE
   ## $2-n : SAME AS echo (MESSAGE WILL BE PREFIXED)
+{
   ecode=$1; shift
   echo; echo -ne "$BP$B_DYING "
   [[ ${1:0:1} == '-' ]] && local opts="$1" && shift
@@ -145,16 +156,16 @@ function bot_fatal () {
 
 ## SCRIPT HAS SOMETHING TO ASK AND WANT A VALID OR EMPTY ANSWER.
 #XXX E.G: bot_choice "are you OK?" 2 yes no 'who knows!'
-function bot_choice () {                  #XXX: FANCIER select ALTERNATIVE
+function bot_choice ()                    #XXX: FANCIER select ALTERNATIVE
   ## $1     : MESSAGE
   ## $2     : DEFAULT (AS INDEX IN $choices) IF USER PROVIDES AN EMPTY ANSWER
   ## $3-n   : CHOICES (WITH UNIQUE LEADING CHARACTER, E.G: "yes" "no" "abort")
   ## returns: 0, AND SETS $BOTASK_ANSWER TO THE ANSWER'S INDEX WITHIN $choices
-
+{
   ## EASE OUR DEVS' LIFE
   [[ $2 =~ ^-?[0-9]+$ ]] || fatal_prog_error "'$2' IS NOT AN INTEGER";
-  local max_i=$(expr $# - $2)
-  [[ $2 -lt $max_i ]] || fatal_prog_error "'$2' > MAX INDEX ($max_i)";
+  local max_i=$(expr $# - 2)
+  [[ $2 -lt $max_i ]] || fatal_prog_error "'$2' >= MAX INDEX ($max_i)";
 
   local msg="$1" def_choice=$2; shift 2
   local choices=("$@")
@@ -185,9 +196,10 @@ function bot_choice () {                  #XXX: FANCIER select ALTERNATIVE
 
 ## SCRIPT WAITS FOR AN EVALED EXPRESSION TO EXIT A NON-ZERO VALUE.
 #XXX E.G: bot_waitWhile test -n \"'$(procname2p \$RE_PROC_httpd)'\"
-function bot_waitWhile () {               #TDL: ADD TIMEOUT?
+function bot_waitWhile ()                 #TDL: ADD TIMEOUT?
   ## $1     : bash EXPRESSION
   ## returns: 0
+{
   local s=0 out=""
 #  set -ex                                #XXX: UNCOMMENT WHEN IT GETS TRICKY.
   while eval $@; do
@@ -206,16 +218,21 @@ function bot_waitWhile () {               #TDL: ADD TIMEOUT?
   return 0
 }
 
-## SCRIPT USES N-th ARG OR ASKS FOR A VALUE
+## SCRIPT USES N-th ARG IF SET OR ASKS FOR A VALUE
 #XXX: E.G: ask_or_arg 0 '"are you OK? " 1 yes no' sure unsure; [[ $?==2 ]] && echo ""
-function bot_argOrChoice () {
+function bot_argOrChoice ()
   ## $1     : N (*INDEX* IN LIST CONSISTING OF ARGUMENTS FROM $3)
   ## $2     : bot_choice COMMAND GIVEN TO eval; E.G: '"proceed? " 1 yes no'
   ## $3-n   : LIST OF ARGUMENTS TO BE INDEXED WITH $1; E.G: $@
   ## echoes : VALUE AT LIST'INDEX OR USER PROVIDED STRING
   ## returns: VALUE ORIGIN: 1 IF FROM ARGUMENTS, 2 IF FROM USER INPUT
+{
+  ## EASE OUR DEVS' LIFE
+  [[ $1 =~ ^-?[0-9]+$ ]] || fatal_prog_error "'$1' IS NOT AN INTEGER";
+
   local i=$1; eval "local bc_args=($2)"; shift 2
   local list=($@)
+
   if [[ -n "${list[$i]}" ]]; then
     echo "${list[$i]}"
     return 1
